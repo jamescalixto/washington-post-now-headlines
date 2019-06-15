9b755b3de36f8cceddb5878991205c7c5daa3fae
jamescalixto <jc2386@cornell.edu>
2019-06-15 17:46:57 -0400

    implemented shuffling

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
+// Given a number n, get k random numbers in the range
+// [0,n) without repetition. Precondition that n >= k.
+function getRandomNumbersWithoutRepetition(n, k) {
+  var random_numbers = [];
+  while (random_numbers.length < k) {
+    let i = Math.floor(Math.random() * n);
+    if (random_numbers.indexOf(i) === -1) {
+      random_numbers.push(i);
+    }
+  }
+  return random_numbers;
+}
+
 // Given an array, gets n random elements without
 // replacement.
 function getRandomElements(n, arr) {
   let random_elements = [];
   let len = arr.length;
-
-  // Given a number n, get k random numbers in the range
-  // [0,n) without repetition. Precondition that n >= k.
-  function getRandomNumbersWithoutRepetition(n, k) {
-    var random_numbers = [];
-    while (random_numbers.length < k) {
-      let i = Math.floor(Math.random() * n) + 1;
-      if (random_numbers.indexOf(i) === -1) {
-        random_numbers.push(i);
-      }
-    }
-    return random_numbers;
-  }
-
   let random_indices = getRandomNumbersWithoutRepetition(len, n);
   for (random_index of random_indices) {
     random_elements.push(arr[random_index]);
   }
   return random_elements;
 }
-
 
 // Given an array of data and the number of headlines to pull,
 // initialize a new puzzle with that number of headlines.
@@ -39,12 +37,14 @@ function startNewPuzzle(n, data) {
   }
   console.log(puzzle_data);
   buildHeadlineElements(puzzle_data);
+  shuffleHeadlineEndElements();
 }
 
 // Given an array of headline elements, build both start and end
 // elements for each.
 function buildHeadlineElements(data) {
-  for (element of data) {
+  for (i in data) {
+    let element = data[i];
     buildHeadlineFromTemplate(true);
     buildHeadlineFromTemplate(false);
 
@@ -52,15 +52,32 @@ function buildHeadlineElements(data) {
     function buildHeadlineFromTemplate(isStart) {
       let side = isStart ? "start" : "end";
       let side_prop = isStart ? element.headline_start : element.headline_end;
-      
+
       // Get and copy template.
-      var headline_template = document.getElementById("headline-" + side + "-element-template");
+      var headline_template = document.getElementById(
+        "headline-" + side + "-element-template"
+      );
       var clone = headline_template.cloneNode(true); // Make copy.
       clone.children.namedItem("headline-text").textContent = side_prop;
-      clone.style.display="inline-block"; // Make copy visible.
+      clone.style.display = "inline-block"; // Make copy visible.
 
       // Add it to the DOM.
-      document.getElementById("headline-" + side + "-container").appendChild(clone)
+      document
+        .getElementById("headline-" + side + "-container")
+        .appendChild(clone);
     }
+  }
+}
+
+// Shuffle headling-end elements.
+// Fisher-Yates variant.
+function shuffleHeadlineEndElements() {
+  var headline_end_container = document.querySelector(
+    "#headline-end-container"
+  );
+  for (var i = headline_end_container.children.length; i >= 0; i--) {
+    headline_end_container.appendChild(
+      headline_end_container.children[(Math.random() * i) | 0]
+    );
   }
 }
